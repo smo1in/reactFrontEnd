280a90c375ec2c9a5c18ec4832643231c0eccf50
Andrey.Smolin (FA.G0) <Andrey.smolin@fsoft.com.vn>
2019-02-25 16:31:42 +0700

    25/2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,5 @@ const Header = () =>{
     return <div className='header'> 
                 <img src ='https://assets.themuse.com/uploaded/companies/468/small_logo.png?v=52a6f3b08df9d999e57db53805c53dc28ac609638a51afd760ad369b11a82491'/>
             </div>
-
 }
-export default Header
+ export default Header
