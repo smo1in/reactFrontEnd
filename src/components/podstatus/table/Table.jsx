a59790b91e23f97505491fa486feb44a75e56afd
Andrey.Smolin (FA.G0) <Andrey.smolin@fsoft.com.vn>
2019-03-04 14:05:39 +0700

    3/4

--- src/components/podstatus/table/Table.jsx
+++ src/components/podstatus/table/Table.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Table.css";
 
 const Table = props => {
-  let tableElements = props.data.map((t, key) => <div>{t.podName}</div>);
+  let tableElements = props.data.map((t, key) => <div>{t.podName},{t.running},{t.stopped},{t.overallState}</div>);
 
   return <div>{tableElements}</div>;
 };
